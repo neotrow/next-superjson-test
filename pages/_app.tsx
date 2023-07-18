66b29563207121e5faae17fe156069e645d2ce83
Timothy Trowbridge <timothy.trowbridge@neolution.ch>
2023-07-18 20:31:25 +0200

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,47 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import App, { AppContext, AppInitialProps, AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type AppOwnProps = {
+  prop1?: string;
+  prop2?: string;
+  prop3?: string;
+};
+
+export function MyCustomApp({
+  Component,
+  pageProps,
+  prop1,
+}: AppProps & AppOwnProps) {
+  return (
+    <>
+      <p>example: {prop1}</p>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+MyCustomApp.getInitialProps = async (
+  context: AppContext
+): Promise<AppOwnProps & AppInitialProps> => {
+  const ctx = await App.getInitialProps(context);
+
+  const ownProps: AppOwnProps = {};
+
+  ownProps.prop1 = "foo";
+  ownProps.prop2 = "bar";
+  ownProps.prop3 = "baz";
+
+  // ownProps { prop1: [Function: withSuperJSON], prop2: 'bar', prop3: 'baz' }
+  console.log("ownProps", ownProps);
+
+  return { ...ctx, ...ownProps };
+
+  // this works but the above return doesn't...
+  // return { ...ctx, prop1: "foo", prop2: "bar", prop3: "baz" };
+
+  // this also works
+  // const result = { ...ctx, prop1: "foo", prop2: "bar", prop3: "baz" };
+  // return result;
+};
+
+export default MyCustomApp;
